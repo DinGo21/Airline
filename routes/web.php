461ca1709d929313e2a4ca7df85cd5434dffe09c
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-01-24 13:19:39 +0100

    feat: added controllers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\FlightController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FlightController::class, "index"])->name("index");
 
 Auth::routes();
 
