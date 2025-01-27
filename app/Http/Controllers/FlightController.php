f083a6494435d047a8aa900daebe4bef4f48c386
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-01-27 15:23:28 +0100

    feat: Added index blade

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Flight;
 use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
     public function index()
     {
-        return view("index");
+        $flights = Flight::All();
+
+        return (view("index", compact("flights")));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,35 @@
 @extends("layouts.app2")
 
 @section("content")
-
+	<input type="text" id="input" class="searchBar" placeholder="">
+	<div class="container">
+		<table id="table" class="table">
+            <thead>
+                <tr>
+                    <th class="label" scope="col">date</th>
+                    <th class="label" scope="col">departure</th>
+                    <th class="label" scope="col">arrival</th>
+                    <th class="label" scope="col">status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($flights as $flight)
+                    <tr class="row">
+                        <td class="cell">
+							<p>{{$flight->date}}</p>
+						</td>
+                        <td class="cell">
+                            <p>{{$flight->departure}}</p>
+                        </td>
+                        <td class="cell">
+							<p>{{$flight->arrival}}</p>
+                        </td>
+                        <td class="cell">
+                            <p>{{$flight->status}}</p>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+	</div>
 @endsection
