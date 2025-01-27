0af64fb6baf1cac858f1a7ece2b73b3e7bfd7654
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-01-27 10:13:59 +0100

    feat: added crud for Api/AirplaneController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AirplaneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post("/airplanes", [AirplaneController::class, "store"])->name("apiairplanestore");
+Route::get("/airplanes", [AirplaneController::class, "index"])->name("apiairplaneindex");
+Route::get("/airplanes/{id}", [AirplaneController::class, "show"])->name("apiairplaneshow");
+Route::put("/airplanes/{id}", [AirplaneController::class, "update"])->name("apiairplaneupdate");
+Route::delete("/airplanes/{id}", [AirplaneController::class, "destroy"])->name("apiairplanedestroy");
