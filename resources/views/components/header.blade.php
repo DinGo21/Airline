78bb7c43333d9357797443dcdb4f6c001c072f35
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-01-27 08:28:46 +0100

    feat: Added components and layout

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+	<a class="logo" href="{{ url('/') }}">
+        <h1>{{config('app.name', 'Airline')}}</h1>
+    </a>
+	<ul class="headerList">
+		<li>Login</li>
+		<li>Register</li>
+	</ul>
+</header>
---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Airline') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/normalizer.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        <x-header />
+        <main class="main">
+            @yield('content')
+        </main>
+        <x-footer />
+    </div>
+</body>
+</html>
