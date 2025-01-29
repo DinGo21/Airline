c25987f11143c51a5833f1797a7fb1415c9baf19
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-01-29 13:31:07 +0100

    style: Added styles to header blade

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,21 +1,30 @@
-<header>
-	<a class="logo" href="{{ url('/') }}">
-        <h1>{{config('app.name', 'Airline')}}</h1>
+<header class="header">
+	<a class="headerLogo" href="{{ url('/') }}">
+        <h1>
+			<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  
+			stroke="#ffffff"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  
+			class="icon icon-tabler icons-tabler-outline icon-tabler-plane-departure"><path stroke="none" 
+			d="M0 0h24v24H0z" fill="none"/><path d="M14.639 10.258l4.83 -1.294a2 2 0 1 1 1.035 3.863l-14.489 
+			3.883l-4.45 -5.02l2.897 -.776l2.45 1.414l2.897 -.776l-3.743 -6.244l2.898 -.777l5.675 5.727z" />
+			<path d="M3 21h18" />
+			</svg>
+			{{config('app.name', 'Airline')}}
+		</h1>
     </a>
 	<ul class="headerList">
 		@guest
 			@if (Route::has('login'))
-				<li>
-					<a href="{{ route('login') }}">{{ __('Login') }}</a>
+				<li class="headerListElement">
+					<a class="headerLink" href="{{ route('login') }}">{{ __('Login') }}</a>
 				</li>
 			@endif
 			@if (Route::has('register'))
-				<li>
-					<a href="{{ route('register') }}">{{ __('Register') }}</a>
+				<li class="headerListElement">
+					<a class="headerLink" href="{{ route('register') }}">{{ __('Register') }}</a>
 				</li>
 			@endif
 		@else
-			<li>
+			<li class="headerListElement">
 				<a href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 					{{ Auth::user()->name }}
 				</a>
