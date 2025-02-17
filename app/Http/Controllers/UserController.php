1ea34520e41e475349f4d3c0f064b77ed9263af5
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-02-17 17:41:17 +0100

    feat: added debook function to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class UserController extends Controller
 {
@@ -13,10 +14,25 @@ class UserController extends Controller
         $this->middleware("auth");
     }
 
-    public function bookings()
+    public function debook(User $user, string $id)
+    {
+        $flight = $user->flights()->where("flight_id", "=", $id)->first();
+        
+        $user->flights()->detach($id);
+        $flight->update([
+            "available_places" => $flight->available_places + 1
+        ]);
+    }
+
+    public function bookings(Request $request)
     {
         $user = Auth::user();
 
+        if ($request->action == "debook" && $request->id)
+        {
+            $this->debook($user, $request->id);
+            return Redirect::to(route("userBookings"));
+        }
         return view("bookings", compact("user"));
     }
 }
--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -33,7 +33,7 @@
                                     <path d="M21 21l-6 -6" />
                                 </svg>
                             </a>
-                            <a class="bookingsOption" href="#">
+                            <a class="bookingsOption" href="{{route('userBookings', ['action' => 'debook', 'id' => $flight->id])}}">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  
                                     stroke="#ff2b2b"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  
                                     class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-x"><path stroke="none"
