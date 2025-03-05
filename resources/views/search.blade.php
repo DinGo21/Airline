a43943ba07d37393371995ce2d2d72f4e54c45cf
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-03-05 15:15:45 +0100

    feat: added alerts for empty results

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -36,16 +36,52 @@
         </div>
     </section>
     <h3 class="fs-2 text-center">Found Results:</h3>
-	<div class="d-flex justify-content-center flex-wrap my-2">
+	<div class="container">
         @foreach ($flights as $flight)
-            <div class="card m-2" style="width: 18rem;">
-                <img src="{{asset('img/show.jpg')}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="fs-3 card-title mb-4">{{$flight->arrival}}</h5>
-                    <p class="card-text fs-5">{{$flight->date}}</p>
-                    <a href="{{route('show', $flight->id)}}" class="btn btn-primary">See Details</a>
+        <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="{{asset('img/show.jpg')}}" class="img-fluid rounded" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">{{$flight->arrival}}</h5>
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="fs-5 text-decoration-underline">Departure:</h4>
+                                    <p>{{$flight->departure}}</p>
+                                </div>
+                                <div class="col">
+                                    <h4 class="fs-5 text-decoration-underline">Date:</h4>
+                                    <p>{{$flight->date}}</p>
+                                </div>
+                                <div class="col">
+                                    <h4 class="fs-5 text-decoration-underline">Plane:</h4>
+                                    <p>{{$flight->airplane->name}}</p>
+                                </div>
+                            </div>
+                            <a href="{{route('show', $flight->id)}}" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         @endforeach
+        @if ($flights->isEmpty())
+            <div class="alert alert-warning my-5 d-flex align-items-center" role="alert">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle flex-shrink-0 me-1" viewBox="0 0 16 16">
+                    <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+                    <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg>
+                Sorry, we were unable to find any results.
+            </div>
+            <div class="d-flex justify-content-center mb-5">
+                <a href="{{ route('index') }}" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                    </svg>
+                    Go back
+                </a>
+            </div>
+        @endif
 	</div>
 @endsection
